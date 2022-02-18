6510ff162e0edd4b053953bb6cd04107c8b1ba6d
TajBanana <ytahjian@gmail.com>
2022-02-18 12:04:24 +0800

    spring boot with generated requests

--- restful/src/main/java/tajbanana/restful/controller/LiveStreamController.java
+++ restful/src/main/java/tajbanana/restful/controller/LiveStreamController.java
@@ -1,21 +1,40 @@
 package tajbanana.restful.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import tajbanana.restful.model.LiveStream;
+import tajbanana.restful.repository.LiveStreamRepo;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping(path = "/streams")
 public class LiveStreamController {
 
+    private final LiveStreamRepo repo;
+
+    public LiveStreamController(LiveStreamRepo repo) {
+        this.repo = repo;
+    }
+
     @GetMapping
     public List<LiveStream> findAll() {
-        streams.add(new LiveStream());
 
-        return null;
+        return repo.findAll();
     }
+
+    @GetMapping(path = "/{Id}")
+    public String findById(@PathVariable String Id) {
+
+        return Id;
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public LiveStream create(@RequestBody LiveStream stream) {
+
+        return repo.create();
+    }
+
 }
--- restful/src/main/java/tajbanana/restful/repository/LiveStreamRepo.java
+++ restful/src/main/java/tajbanana/restful/repository/LiveStreamRepo.java
@@ -1,14 +1,24 @@
 package tajbanana.restful.repository;
 
+import org.springframework.stereotype.Repository;
 import tajbanana.restful.model.LiveStream;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Repository
 public class LiveStreamRepo {
     List<LiveStream> streams =  new ArrayList<>();
 
     public LiveStreamRepo() {
         streams.add(new LiveStream());
+    }
+
+    public List<LiveStream> findAll() {
+        return null;
+    }
+
+    public LiveStream create() {
+        return null;
     }
 }
